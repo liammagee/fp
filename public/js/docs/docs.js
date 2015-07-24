92b25519ddd4d7095a6e988ba4c1aeb61c04fc4e
Liam Magee <liam.magee@gmail.com>
2015-07-24 23:14:21 +1000

    Update doc references

--- public/js/docs/docs.js
+++ public/js/docs/docs.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
 	$('.links').click(function(e) {
 		var id = e.target.id;
+        var parts = id.split('-');
+        var len = parts.length;
+        var key = parts[ 0 ];
+        var folder = parts[ 1 ];
+        var name = parts.slice( 2, len ).join( '-' );
+
         $('.heading').hide();
         $('#heading-' + id).show();
         $('.content').hide();
         $('#content-' + id).show();
+        $('.content-group').hide();
+        $('#content-group-intro-' + folder).show();
         $('#model').empty();
 
-        var parts = id.split('-');
-
-        if ( parts.length >= 2 && parts[ 0 ] === "example" ) {
-            var len = parts.length;
-            var folder = parts[ 1 ];
-            var name = parts.slice( 2, len ).join( '-' );
+        if ( key === "example" ) {
             $('#model').append('<iframe frameborder="0" src="examples/' + folder + '/' + name + '.html"/>');
         }
 	});
